2e2ce680a9d98cc72acb8682e1309e598e2840b8
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-22 21:07:02 +0300

    created the trackin page

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router-dom";
+import socks from "../assets/images/products/athletic-cotton-socks-6-pairs.jpg"
+
 function Tracking(props) {
 
-    return <div>
+    return <div className="p-10">
+      <div className="">
+    <Link to="/pages/Order"  className="underline text-green-600 ">View all orders</Link>
+          
+        
 
+        <div className="font-bold text-2xl mt-4">
+          Arriving on Monday, June 13
+        </div>
+
+        <div className="">
+          Black and Gray Athletic Cotton Socks - 6 Pairs
+        </div>
+
+        <div className="">
+          Quantity: 1
+        </div>
+            <div className="w-50">
+                <img className="w-full" src={socks} />
+            </div>
+        
+
+        <div className="flex justify-between fon">
+          <div className="font-bold text-2xl">
+            Preparing
+          </div>
+          <div className="font-bold text-2xl text-green-700">
+            Shipped
+          </div>
+          <div className="font-bold text-2xl">
+            Delivered
+          </div>
+        </div>
+
+        <div className="h-5 border rounded-xl">
+          <div className="w-1/2 bg-green-700 h-full rounded-xl"></div>
+        </div>
+      </div>
     </div>
 }
 
