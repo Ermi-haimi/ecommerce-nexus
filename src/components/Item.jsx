00b066bef9b1ffa22cc542453356a86d84dc6de4
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-22 16:35:00 +0300

    created the home page and items icon

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,29 @@
 function Item(props) {
 
-    return <div>
-
+    return <div className="flex flex-col border items-center p-5">
+        <div className="p-3">
+            <img src={props.imageSource} alt={ props.imageName} className="rounded-xl" />
+        </div>
+        <div className="flex flex-col gap-2">
+            <h2>{props.itemDescription}</h2>
+            <div className=""><img src={props.ratingIcon} alt="Rating" /> <p>{ props.ratingValue}</p></div>
+            <p>{props.price}</p>
+            <div className="">
+            <select className="border">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+            <button className="bg-green-700 text-white p-1">Add to Cart</button>
+        </div>
     </div>
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
-
+import Item from "../components/Item";
+import socks from "../assets/images/products/athletic-cotton-socks-6-pairs.jpg"
+import basketball from "../assets/images/products/intermediate-composite-basketball.jpg"
+import tshirt from "../assets/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"
+import rating1 from "../assets/images/ratings/rating-45.png";
+import rating2 from "../assets/images/ratings/rating-40.png";
 
 function Home(props) {
 
-    return <div>
-
+    return <div className="flex">
+        <Item itemDescription="Black and Gray Athletic Cotton Socks - 6 Pairs" imageSource={socks} ratingIcon={ rating1} ratingValue={87} price="$10.90"/>
+        <Item itemDescription="Intermediate Size Basketball" imageSource={basketball} ratingIcon={rating2} ratingValue={127} price="$20.95"/>
+        <Item itemDescription="Adults Plain Cotton T-Shirt - 2 Pack" imageSource={tshirt} ratingIcon={ rating1} ratingValue={56} price="$7.99"/>
     </div>
 }
 
