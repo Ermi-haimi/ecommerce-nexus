2c2b2ac3c265aa412883dab3045ef981395e4bad
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-23 08:22:45 +0300

    created the checkout page (left with styling)

--- src/components/ItemCheckout.jsx
+++ src/components/ItemCheckout.jsx
@@ -1,8 +1,76 @@
 function ItemCheckout(props) {
 
-    return <div>
+    return <div className="">
+              <p>Delivery date {props.deliveryDate}</p>
+            
 
-    </div>
+            <div className="">
+              <img className="w-50"
+                src={props.imageSource} />
+
+              <div className="">
+                <div className="">
+                  {props.itemName}
+                </div>
+                <div className="">
+                  {props.price}
+                </div>
+                <div className="">
+                  <span>Quantity: <span className="">{ props.quantity}</span>
+                  </span>
+                  <span className="">
+                    Update
+                  </span>
+                  <span className="">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div className="">
+                <div className="e">
+                  Choose a delivery option:
+                </div>
+
+                <div className="">
+                  <input type="radio" className=""
+                    name="delivery-option-2" />
+                  <div>
+                    <div className="">
+                      Tuesday, June 21
+                    </div>
+                    <div className="">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div className="">
+                  <input type="radio" checked className=""
+                    name="delivery-option-2" />
+                  <div>
+                    <div className="ion-date">
+                      Wednesday, June 15
+                    </div>
+                    <div className="ion-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div className="">
+                  <input type="radio" className=""
+                    name="delivery-option-2" />
+                  <div>
+                    <div className="">
+                      Monday, June 13
+                    </div>
+                    <div className="">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 }
 
 export default ItemCheckout;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,45 @@
+import ItemCheckout from "../components/ItemCheckout";
+import socks from "../assets/images/products/athletic-cotton-socks-6-pairs.jpg"
+import basketball from "../assets/images/products/intermediate-composite-basketball.jpg"
 
 function Checkout(props) {
+    const st = "flex justify-between "
+    return <div className="p-6">
+            <h2 className="font-bold text-2xl mb-7">Review your order</h2>
+        <div className="border flex flex-col p-4 gap-3">
+            <h3 className="font-bold text-xl">Payment Summary</h3>
+                <div className={st}>
+                    <div>Items (3):</div>
+                    <div className="">$42.75</div>
+                </div>
 
-    return <div>
+                <div className={st}>
+                    <div>Shipping &amp; handling:</div>
+                    <div className="">$4.99</div>
+                </div>
 
-    </div>
+                <div className={st}>
+                    <div>Total before tax:</div>
+                    <div className="">$47.74</div>
+                </div>
+
+                <div className={st}>
+                    <div>Estimated tax (10%):</div>
+                    <div className="">$4.77</div>
+                </div>
+<hr />
+                <div className="flex justify-between text-green-600 font-bold text-xl">
+                    <div>Order total:</div>
+                    <div className="">$52.51</div>
+                </div>
+
+                    <button className="bg-green-700 p-2">
+                    Place your order
+                    </button>
+            </div>
+            <ItemCheckout deliveryDate="Tuesday, June 21" imageSource={ socks} itemName="Black and Gray Athletic Cotton Socks - 6 Pairs" price="$10.90" quantity="2"  />
+            <ItemCheckout deliveryDate="Wednesday, June 15" imageSource={ basketball} itemName="Intermediate Size Basketball" price="$20.95" quantity="1"   />
+        </div>
 }
 
 export default Checkout;
