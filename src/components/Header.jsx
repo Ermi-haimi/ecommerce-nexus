f6dbfa215f916d95d836ccd3652ae1fc66974542
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-23 10:42:52 +0300

    styled the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
+import cart from "../assets/images/icons/cart-icon.png";
+import logo from "../assets/images/logo-white.png"
+import arrow from "../assets/images/icons/search-icon.png"
 
 
 function Header() {
 
-    return <div className="flex justify-around bg-green-800 p-5 ">
-        <Link to="/" className="text-white">Home</Link>
-        <Link to="/pages/Order" className="text-white">Order</Link>
-        <Link to="/pages/Checkout" className="text-white">Checkout</Link>
-        <Link to="/pages/Tracking" className="text-white">Tracking</Link>
+    return <div className="flex justify-around bg-green-900 p-5 items-center gap-3  ">
+        <Link to="/" className="text-white"><img src={logo} className="w-40 hover:border p-2" alt="" /></Link>
+        <div className="flex flex-1 justify-center">
+            <input type="text" className="bg-white p-2 h-11 rounded-sm rounded-r-none w-full max-w-[850px] " placeholder="Search" />
+            <div className="p-2 w-10  bg-green-200 rounded-r-sm"><img src={ arrow} className=" "/></div>
+            
+        </div>
+        
+        <Link to="/pages/Order" className="text-white font-bold hover:border p-1 h-full">Order</Link>
+        <Link to="/pages/Checkout" className="text-white">
+            <div className="flex hover:border p-1 relative">
+                <div>
+                    <img src={cart} className="w-10" alt="" />
+                    <p className="absolute top-1 text-green-900 left-6 font-bold">3</p>
+                </div>
+                
+                <p className="font-bold">Cart</p>
+            </div></Link>
     </div>
 }
 
