41a5ba1e077eb84cce10cf4342acd8876a513ca9
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-23 09:41:51 +0300

    finished the checkout page

--- src/components/ItemCheckout.jsx
+++ src/components/ItemCheckout.jsx
@@ -1,75 +1,75 @@
 function ItemCheckout(props) {
 
-    return <div className="">
-              <p>Delivery date {props.deliveryDate}</p>
-            
-
-            <div className="">
-              <img className="w-50"
+  return <div className="p-5 border border-gray-300 rounded-sm">
+    <p className="font-bold text-green-600 text-xl ">Delivery date {props.deliveryDate}</p>
+    <div className=" border-gray-400 p-5">
+            <div className=" gap-y-6 grid grid-cols-[140px_1fr] lg:grid-cols-[140px_1fr_1fr] lg:gap-2">
+              <img className="w-30 "
                 src={props.imageSource} />
 
-              <div className="">
-                <div className="">
+              <div className=" ">
+                <div className="font-bold">
                   {props.itemName}
                 </div>
                 <div className="">
                   {props.price}
                 </div>
-                <div className="">
+                <div className="*:mr-3">
                   <span>Quantity: <span className="">{ props.quantity}</span>
                   </span>
-                  <span className="">
+                  <span className="text-green-500 hover:text-green-400 cursor-pointer">
                     Update
                   </span>
-                  <span className="">
+                  <span className="text-green-500 hover:text-green-400 cursor-pointer">
                     Delete
                   </span>
                 </div>
-              </div>
+            </div>
 
-              <div className="">
-                <div className="e">
+              <div className="w-60">
+                <div className="font-bold">
                   Choose a delivery option:
                 </div>
 
-                <div className="">
-                  <input type="radio" className=""
-                    name="delivery-option-2" />
-                  <div>
-                    <div className="">
+                <div className="flex gap-4">
+                  <input type="radio" className="w-[18px]"
+                    name={props.radioName} />
+                  <div className="">
+                    <div className="font-bold">
                       Tuesday, June 21
                     </div>
-                    <div className="">
+                    <div className="text-gray-500">
                       FREE Shipping
                     </div>
                   </div>
                 </div>
-                <div className="">
-                  <input type="radio" checked className=""
-                    name="delivery-option-2" />
+                <div className="flex gap-4">
+                  <input type="radio"  className="w-[18px]"
+                    name={props.radioName} />
                   <div>
-                    <div className="ion-date">
+                    <div className="font-bold">
                       Wednesday, June 15
                     </div>
-                    <div className="ion-price">
+                    <div className="text-gray-500">
                       $4.99 - Shipping
                     </div>
                   </div>
                 </div>
-                <div className="">
-                  <input type="radio" className=""
-                    name="delivery-option-2" />
+                <div className="flex gap-4">
+                  <input type="radio" className="w-[18px]"
+                    name={props.radioName} />
                   <div>
-                    <div className="">
+                    <div className="font-bold">
                       Monday, June 13
                     </div>
-                    <div className="">
+                    <div className="text-gray-500">
                       $9.99 - Shipping
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+      </div>
+      </div>
           </div>
 }
 
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,41 +5,43 @@ import basketball from "../assets/images/products/intermediate-composite-basketb
 function Checkout(props) {
     const st = "flex justify-between "
     return <div className="p-6">
-            <h2 className="font-bold text-2xl mb-7">Review your order</h2>
-        <div className="border flex flex-col p-4 gap-3">
-            <h3 className="font-bold text-xl">Payment Summary</h3>
-                <div className={st}>
-                    <div>Items (3):</div>
-                    <div className="">$42.75</div>
+        <h2 className="font-bold text-2xl mb-7">Review your order</h2>
+        <div className="p-6 grid gap-6 max-w-6xl m-auto lg:grid-cols-[1fr_300px] "> 
+            <ItemCheckout deliveryDate="Tuesday, June 21" imageSource={socks} itemName="Black and Gray Athletic Cotton Socks - 6 Pairs" price="$10.90" quantity="2" radioName="delivery-1" />
+            <div className="border border-gray-300 flex flex-col p-4 gap-3 w-full -order-1 lg:order-0 rounded-sm ">
+                <h3 className="font-bold text-xl">Payment Summary</h3>
+                    <div className={st}>
+                        <div>Items (3):</div>
+                        <div className="">$42.75</div>
+                    </div>
+
+                    <div className={st}>
+                        <div>Shipping &amp; handling:</div>
+                        <div className="">$4.99</div>
+                    </div>
+
+                    <div className={st}>
+                        <div>Total before tax:</div>
+                        <div className="">$47.74</div>
+                    </div>
+
+                    <div className={st}>
+                        <div>Estimated tax (10%):</div>
+                        <div className="">$4.77</div>
+                    </div>
+                    <hr className="text-gray-300" />
+                    <div className="flex justify-between text-green-600 font-bold text-xl">
+                        <div>Order total:</div>
+                        <div className="">$52.51</div>
                 </div>
 
-                <div className={st}>
-                    <div>Shipping &amp; handling:</div>
-                    <div className="">$4.99</div>
-                </div>
-
-                <div className={st}>
-                    <div>Total before tax:</div>
-                    <div className="">$47.74</div>
-                </div>
-
-                <div className={st}>
-                    <div>Estimated tax (10%):</div>
-                    <div className="">$4.77</div>
-                </div>
-<hr />
-                <div className="flex justify-between text-green-600 font-bold text-xl">
-                    <div>Order total:</div>
-                    <div className="">$52.51</div>
-                </div>
-
-                    <button className="bg-green-700 p-2">
+                    <button className="bg-green-700 p-2 text-white rounded-sm hover:opacity-70">
                     Place your order
                     </button>
             </div>
-            <ItemCheckout deliveryDate="Tuesday, June 21" imageSource={ socks} itemName="Black and Gray Athletic Cotton Socks - 6 Pairs" price="$10.90" quantity="2"  />
-            <ItemCheckout deliveryDate="Wednesday, June 15" imageSource={ basketball} itemName="Intermediate Size Basketball" price="$20.95" quantity="1"   />
-        </div>
+            <ItemCheckout deliveryDate="Wednesday, June 15" imageSource={ basketball} itemName="Intermediate Size Basketball" price="$20.95" quantity="1"  radioName="delivery-2" />
+    </div>
+    </div>
 }
 
 export default Checkout;
