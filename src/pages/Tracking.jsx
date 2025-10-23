9738eb29098100656708d04a954369900f136f7c
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-23 10:53:44 +0300

    add a max-width

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -3,7 +3,7 @@ import socks from "../assets/images/products/athletic-cotton-socks-6-pairs.jpg"
 
 function Tracking(props) {
 
-    return <div className="p-10">
+    return <div className="p-10 max-w-[900px] m-auto">
       <div className="">
     <Link to="/pages/Order"  className="underline text-green-600 ">View all orders</Link>
           
@@ -38,7 +38,7 @@ function Tracking(props) {
         </div>
 
         <div className="h-5 border rounded-xl">
-          <div className="w-1/2 bg-green-700 h-full rounded-xl"></div>
+          <div className="w-1/2 bg-green-700 h-full opacity-80 rounded-xl"></div>
         </div>
       </div>
     </div>
